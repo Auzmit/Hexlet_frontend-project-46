a0cc7c10e0e92ed53a0cde5aecf2d8c733e77ebe
andy <atrudkov@mail.ru>
2023-04-16 19:17:34 +0300

    half of the 3-rd step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,10 @@
 import { program } from 'commander';
 
 program
-  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  //.option('-V, --version', 'output the version number')
   // .option('-h, --help', 'display help for command')
   .parse();
-  
