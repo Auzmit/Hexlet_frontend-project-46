85d66094c98bae60a0fb7afa54c01a389a9d466a
andy <atrudkov@mail.ru>
2023-04-19 15:23:18 +0300

    trying to maintanbility to A, but failed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
-import stylish from './stylish.js';
+import { /* giveTypesToDiff, */ stylishedDiff } from './stylish.js';
 
 function buildPath(relativePath) { return (path.resolve(process.cwd(), relativePath)); }
 function getData(absolutePath) { return (JSON.parse(fs.readFileSync(absolutePath), 'utf-8')); }
+/* function giveTypesToDiff(sortedKeys) {
+  return (sortedKeys.map((key) => {
+      if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
+        return { key, type: 'added', value: data2[key] };
+      }
+      if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
+        return { key, type: 'deleted', value: data1[key] };
+      }
+      if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key) && data1[key] !== data2[key]) {
+        return { key, type: 'edited', value: [data1[key], data2[key]] };
+      }
+      return { key, type: 'unchanged', value: data2[key] };
+    }))
+} */
 
 export default function gendiff(path1, path2) {
   const data1 = getData(buildPath(path1));
@@ -26,5 +40,7 @@ export default function gendiff(path1, path2) {
     return { key, type: 'unchanged', value: data2[key] };
   });
 
-  return stylish(diff);
+  // const diffWithTypes = giveTypesToDiff(sortedKeys);
+
+  return stylishedDiff(diff);
 }
--- src/stylish.js
+++ src/stylish.js
@@ -1,4 +1,19 @@
-export default function stylish(diff) {
+/* export function giveTypesToDiff(sortedKeys) {
+  return (sortedKeys.map((key) => {
+      if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
+        return { key, type: 'added', value: data2[key] };
+      }
+      if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
+        return { key, type: 'deleted', value: data1[key] };
+      }
+      if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key) && data1[key] !== data2[key]) {
+        return { key, type: 'edited', value: [data1[key], data2[key]] };
+      }
+      return { key, type: 'unchanged', value: data2[key] };
+    }))
+} */
+
+export function stylishedDiff(diff) {
   const stylishedDiff = diff.reduce((acc, elem) => {
     const { key } = elem;
     const { value } = elem;
