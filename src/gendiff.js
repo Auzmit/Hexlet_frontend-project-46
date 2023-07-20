fab697429d2aae08af437c8d0ff8907f3c2c16a3
andy <atrudkov@mail.ru>
2023-07-20 20:01:32 +0300

    why 'export default' otdel'no?

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,12 +2,10 @@ import findDifferences from './findDifferences.js';
 import formatter from './formatters/index.js';
 import parser from './parser.js';
 
-const gendiff = (filePath1, filePath2, formatType = 'stylish') => {
+export default function gendiff(filePath1, filePath2, formatType = 'stylish') {
   const firstFile = parser(filePath1);
   const secondFile = parser(filePath2);
   const differences = findDifferences(firstFile, secondFile);
   const formatedFile = formatter(differences, formatType);
   return formatedFile;
-};
-
-export default gendiff;
+}
